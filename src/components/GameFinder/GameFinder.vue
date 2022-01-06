076aa208680df031fe693e25106033a52e9c65a3
Daniel Powley <danpowley@gmail.com>
2022-01-06 21:49:57 +0000

    Basic team selection interactivity

--- src/components/GameFinder/GameFinder.vue
+++ src/components/GameFinder/GameFinder.vue
@@ -4,11 +4,11 @@
     <div class="header">Select Teams</div>
     <div class="content" id="teamswrapper">
       <div class="lfgList">
-        <div class="lfgteam" v-for="lfgTeam in lfgTeams" :key="lfgTeam.id">
-          <input class="teamcheck" type="checkbox" v-model="selectedTeamIds" :value="lfgTeam.id">
-          <img :src="'https://fumbbl.com/i/' + lfgTeam.raceLogos[0].logo" />
-          <div class="teamname">{{ lfgTeam.name }}</div>
-          <div class="teaminfo">TV {{ lfgTeam.teamValue/1000 }}k {{ lfgTeam.race }}</div>
+        <div class="lfgteam" v-for="canLfgTeam in canLfgTeams" :key="canLfgTeam.id">
+          <input class="teamcheck" type="checkbox" v-model="selectedIsLfgTeamIds" :value="canLfgTeam.id">
+          <img :src="'https://fumbbl.com/i/' + canLfgTeam.raceLogos[0].logo" />
+          <div class="teamname">{{ canLfgTeam.name }}</div>
+          <div class="teaminfo">TV {{ canLfgTeam.teamValue/1000 }}k {{ canLfgTeam.race }}</div>
         </div>
       </div>
       <div class="controls">
@@ -16,42 +16,43 @@
             <input type="checkbox" id="allteams" v-model="allTeams" @change="toggleAllTeams" />
             <label for="allteams">Select All</label>
         </div>
-        <button @click="showLfg">Done</button>
+        <button @click="confirmCoachTeamsIsLfg">Done</button>
       </div>
     </div>
   </div>
   <div class="basicbox" v-show="display == 'lfg'">
     <div class="header">Game Finder</div>
     <div class="content" id="wrapper">
+
       <div class="teamsheader">
         <div class="title">Teams</div>
       </div>
+
       <div class="teams">
-        <div class="title">My Teams <span class="smalltitletext">(<a href="#" @click="showTeams">Choose teams</a>)</span></div>
-        <div class="team team--selected">
-          <div class="arrow-right"></div>
-          <div class="teamname">Bobbins team</div>
-          <div class="teamracevalue"><em>1000k Orc</em></div>
+
+        <div class="title">My Teams <span class="smalltitletext">(<a href="#" @click="showCoachTeamsIsLfg">Choose teams</a>)</span></div>
+
+        <div v-show="isLfgTeams.length === 0">
+          No teams selected.
         </div>
-        <div class="team">
-          <div class="arrow-right"></div>
-          <div class="teamname">Great team</div>
-          <div class="teamracevalue"><em>1100k Skaven</em></div>
-        </div>
-        <div class="team">
-          <div class="arrow-right"></div>
-          <div class="teamname">Amazing team</div>
-          <div class="teamracevalue"><em>1400k Goblins</em></div>
-        </div>
-        <div class="team">
-          <div class="arrow-right"></div>
-          <div class="teamname">Awesome team</div>
-          <div class="teamracevalue"><em>1700k Ogres</em></div>
-        </div>
+
+        <template v-for="team in isLfgTeams">
+          <div class="team" :key="team.id" @click="chooseOpponents(team)" :class="{'team--selected': chosenTeam !== null && chosenTeam.id === team.id}">
+            <div class="arrow-right"></div>
+            <div class="teamname">{{ team.name }}</div>
+            <div class="teamracevalue"><em>TV {{ team.teamValue/1000 }}k {{ team.race }}</em></div>
+          </div>
+        </template>
+
       </div>
+
       <div class="opponents">
-        <div class="title">Opponents</div>
-        <div class="opponent">
+
+        <div class="title">Opponents <span class="smalltitletext">({{ chosenTeamName }})</span></div>
+
+        <div v-show="this.loadingOpponents" class="loader"></div>
+
+        <div class="opponent" :class="{ reloading: this.loadingOpponents }">
           <div class="coachheader">
             Jonno (Legend)
           </div>
@@ -68,7 +69,7 @@
             </div>
           </div>
         </div>
-        <div class="opponent">
+        <!-- <div class="opponent">
           <div class="coachheader">
             Greggo (Legend)
           </div>
@@ -84,8 +85,10 @@
               <div class="offerstatusinfo"><span class="statuslabel statuslabel--offersent">Offer sent</span></div>
             </div>
           </div>
-        </div>
+        </div> -->
+
       </div>
+
       <div class="matchoffers">
 
         <div class="matchoffersheader">
@@ -161,12 +164,126 @@ import { PropType } from 'vue';
 // import axios from 'axios'
 import { Coach } from '@/interfaces'
 
-interface lfgTeam {
+function getRandomInteger(max: number): number {
+  return Math.floor(Math.random() * max)
+}
+
+function getRandomId(): number {
+  return getRandomInteger(1000000)
+}
+
+type Race = 'High Elf' | 'Goblin' | 'Tomb Kings' | 'Chaos Dwarf'
+type YesNo = 'Yes' | 'No'
+type TeamStatus = 'Active' | 'New' | 'Retired'
+
+interface RaceLogo {
+  size: number,
+  logo: number
+}
+
+interface Team {
+  id: number,
+  coachId: number,
+  name: string,
+  rosterId: number,
+  race: Race,
+  teamValue: number,
+  canLfg: YesNo,
+  isLfg: YesNo,
+  games: string,
+  divisionId: number,
+  division: string,
+  leagueId: number,
+  league: null,
+  status: TeamStatus,
+  raceLogos: RaceLogo[]
+}
+
+interface CoachTeams {
   id: number,
   name: string,
-  teamValue: number,
-  race: string,
-  raceLogos: [{logo: number}]
+  teams: Team[],
+}
+
+const api = {
+  coachTeamsIsLfg: function (teamIds: number[]) {
+    console.log('API: /coach/teams/islfg (new api method', teamIds)
+  },
+  coachTeams: function (coachName: string, cheatOnlyIsLfgTeamIds: number[]): CoachTeams {
+    console.log('API: /coach/teams/{coach}', coachName)
+    return {
+      id: getRandomId(),
+      name: coachName,
+      teams: [
+        {
+          id: 1051434,
+          coachId: 257339,
+          name: "Suave and Soulless",
+          rosterId: 5145,
+          race: "Tomb Kings",
+          teamValue: 1430000,
+          canLfg: "Yes",
+          isLfg: cheatOnlyIsLfgTeamIds.includes(1051434) ? 'Yes' : 'No', // CHEAT
+          games: "16",
+          divisionId: 2,
+          division: "Competitive",
+          leagueId: 0,
+          league: null,
+          status: "Active",
+          raceLogos: [
+            {
+              size: 32,
+              logo: 486296
+            },
+          ]
+        },
+        {
+          id: 1054459,
+          coachId: 257339,
+          name: "Furious Mojo",
+          rosterId: 5142,
+          race: "Chaos Dwarf",
+          teamValue: 1080000,
+          canLfg: "Yes",
+          isLfg: cheatOnlyIsLfgTeamIds.includes(1054459) ? 'Yes' : 'No', // CHEAT
+          games: "3",
+          divisionId: 2,
+          division: "Competitive",
+          leagueId: 0,
+          league: null,
+          status: "Active",
+          raceLogos: [
+            {
+              size: 32,
+              logo: 486248
+            },
+          ]
+        },
+        {
+          id: 1058152,
+          coachId: 257339,
+          name: "Day Is Done",
+          rosterId: 5145,
+          race: "Tomb Kings",
+          teamValue: 980000,
+          canLfg: "Yes",
+          isLfg: cheatOnlyIsLfgTeamIds.includes(1058152) ? 'Yes' : 'No', // CHEAT
+          games: "0",
+          divisionId: 2,
+          division: "Competitive",
+          leagueId: 0,
+          league: null,
+          status: "Active",
+          raceLogos: [
+            {
+              size: 32,
+              logo: 486296
+            },
+          ]
+        },
+      ]
+    }
+  }
 }
 
 export default Vue.extend({
@@ -183,48 +300,70 @@ export default Vue.extend({
     return {
       display: 'lfg',
       allTeams: false,
-      lfgTeams: [
-        {
-          id: 1,
-          name: 'Bobbins team',
-          teamValue: 1500000,
-          race: 'Orc',
-          raceLogos: [{logo: 486272}]
-        },
-        {
-          id: 2,
-          name: 'Mighty team',
-          teamValue: 1600000,
-          race: 'Skaven',
-          raceLogos: [{logo: 486272}]
-        },
-      ] as lfgTeam[],
-      selectedTeamIds: [] as number[],
+      teams: [] as Team[],
+      selectedIsLfgTeamIds: [] as number[],
+      chosenTeam: null as Team | null,
+      loadingOpponents: false,
       pollingIntervalId: undefined as number | undefined,
     }
   },
   computed: {
-  },
-  methods: {
-    showLfg() {
-      this.display = 'lfg'
-    },
-    showTeams() {
-      this.display = 'teams'
-    },
-    toggleAllTeams() {
-      this.selectedTeamIds = []
-      if (this.allTeams) {
-        for (const lfgTeam of this.lfgTeams) {
-          this.selectedTeamIds.push(lfgTeam.id)
+    canLfgTeams(): Team[] {
+      const canLfgTeams = []
+      for (const team of this.teams) {
+        if (team.canLfg === 'Yes' && team.status === 'Active') {
+          canLfgTeams.push(team)
         }
       }
+      return canLfgTeams
+    },
+    isLfgTeams(): Team[] {
+      const isLfgTeams = []
+      for (const team of this.teams) {
+        if (team.isLfg === 'Yes' && team.status === 'Active') {
+          isLfgTeams.push(team)
+        }
+      }
+      return isLfgTeams
+    },
+    chosenTeamName(): string {
+      return this.chosenTeam ? this.chosenTeam.name : 'Please choose team'
+    }
+  },
+  methods: {
+    showCoachTeamsIsLfg() {
+      this.display = 'teams'
+    },
+    confirmCoachTeamsIsLfg() {
+      api.coachTeamsIsLfg(this.selectedIsLfgTeamIds)
+      const coachTeamResult = api.coachTeams(this.coach.name, this.selectedIsLfgTeamIds)
+      this.teams = coachTeamResult.teams
+      this.chosenTeam = null
+      this.display = 'lfg'
+    },
+    toggleAllTeams() {
+      this.selectedIsLfgTeamIds = []
+      if (this.allTeams) {
+        for (const canLfgTeam of this.canLfgTeams) {
+          this.selectedIsLfgTeamIds.push(canLfgTeam.id)
+        }
+      }
+    },
+    chooseOpponents(team: Team) {
+      this.chosenTeam = team
+      this.loadingOpponents = true
+      setTimeout(function (this: {loadingOpponents: boolean}): void {
+        this.loadingOpponents = false
+      }.bind(this), 1000)
     }
   },
   created: function (): void {
     // this.pollingIntervalId = setInterval(function (this: {}): void {
     //   // call fake api
     // }.bind(this), 2000)
+
+    const coachTeamResult = api.coachTeams(this.coach.name, this.selectedIsLfgTeamIds)
+    this.teams = coachTeamResult.teams
   },
   destroyed: function (): void {
     clearInterval(this.pollingIntervalId)
@@ -312,7 +451,7 @@ export default Vue.extend({
 
 #wrapper {  display: grid;
   grid-template-columns: 1.2fr 0.9fr 0.9fr;
-  grid-template-rows: 1fr auto;
+  grid-template-rows: auto 1fr;
   gap: 10px 10px;
   grid-auto-flow: row;
   grid-template-areas:
@@ -417,22 +556,30 @@ export default Vue.extend({
   background-color: white;
   border-width: 1px;
   border: solid #cec4ac 1px;
+  border-left: solid #cec4ac 10px;
   padding: 4px;
   cursor: pointer;
   margin-bottom: 4px;
 }
 
 #wrapper .teams .team:hover, #wrapper .teams .team--selected {
-  background-color: #d8f2d8;
   border-color: #090;
+}
+
+#wrapper .teams .team:hover {
+  background-color: #d8f2d8;
+}
+
+#wrapper .teams .team--selected:hover {
+  background-color: white;
 }
 
 #wrapper .teams .team .arrow-right {
   width: 0;
   height: 0; 
-  border-top: 5px solid transparent;
-  border-bottom: 5px solid transparent;
-  border-left: 11px solid #090;
+  border-top: 7px solid transparent;
+  border-bottom: 7px solid transparent;
+  border-left: 7px solid #090;
   margin: auto 0;
   float: right;
   display: none;
@@ -451,6 +598,10 @@ export default Vue.extend({
 
 #wrapper .opponent {
   margin-bottom: 20px;
+}
+
+.gamefinder .opponent.reloading {
+  opacity: 0.2;
 }
 
 #wrapper .opponent .coachheader {
